0fea055ec646aab74124b21f599fd1014145f33f
lepean14 <lepean14@b048-18.crio.dauphine.fr>
2016-03-16 17:02:22 +0100

    ex12 done

--- ex12.R
+++ ex12.R
@@ -9,13 +9,13 @@ P_2_birthday<-function(n){
   return (1- (prod((365-n+1):365)) /365^n)
 }
 
-p<-1:365
+p<-2:365
 for(i in p){
-  p[i]<-P_2_birthday(i)
+  p[i-1]<-P_2_birthday(i)
 }
 
 i<-2
-while (i<=365 && p[i]<=0.5){i=i+1}
+while (i<=365 && p[i-1]<=0.5){i=i+1}
 
 print(i)
-plot(1:365,p,type='l')
+plot(2:365,p,type='l')
