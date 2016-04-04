79cac63f529a352f40f1c3566252eb74e283e793
LAntoine <lepersantoine80@gmail.com>
2016-04-04 22:44:08 +0200

    reject method for one element non tested

--- ex14.R
+++ ex14.R
@@ -1,18 +1,22 @@
 rm(list=ls())
 
-#exemple de methode de rejet pour poisson
-lambda=2 #on fixe une valeur arbitraire de lambda
-t=1 #on fixe une valeur arbitraire de t
-b=0
-s=0
-n=-1
-while (b==0)
-{
-  n=n+1
-  s=s-log(runif(1,0,1))/lambda
-  if (s>t)
-  {
-    b=1
+fc_dens<-function(x){
+  if (x<0 || x>1)
+    return (0)
+  return (exp(x)/(exp(1)-1))
+}
+
+#je me suis inspiré de l'algorithme de Wikipedia
+#je vais faire la methode de rejet pour une valeur à la fois pour commencer
+#je ne l'ai pas encore testé
+
+# f=Ce^x où C=1/(e-1)
+# g=unif(0,1)
+b = TRUE
+while (b) {
+  y<-runif(1,0,1)
+  u<-runif(1,0,1)
+  if (u<=f(y)/y){
+    b<-FALSE
   }
 }
-print(n)
