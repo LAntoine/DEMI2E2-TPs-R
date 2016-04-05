b6ffd7f0c5f59e420a22b50abbc661ea99adf3fd
lepean14 <lepean14@b026-07.crio.dauphine.fr>
2016-04-05 11:32:50 +0200

    ex14 which seems to be working

--- ex14.R
+++ ex14.R
@@ -5,18 +5,32 @@ fc_dens<-function(x){
     return (0)
   return (exp(x)/(exp(1)-1))
 }
-
+#la fonction de densité ne va jamais au dessus de 1.6
+#on utilisera donc une loi uniforme entre 0 et 1
 #je me suis inspiré de l'algorithme de Wikipedia
 #je vais faire la methode de rejet pour une valeur à la fois pour commencer
 #je ne l'ai pas encore testé
 
-# f=Ce^x où C=1/(e-1)
-# g=unif(0,1)
-b = TRUE
-while (b) {
-  y<-runif(1,0,1)
-  u<-runif(1,0,1)
-  if (u<=f(y)/y){
-    b<-FALSE
-  }
+fc_methode_rejet<-function(n){
+  # f=Ce^x où C=1/(e-1)
+  # g=unif(0,1)
+    y<-runif(n,0,1)
+    u<-runif(n,0,1)
+    for (i in 1:n){
+      while (u[i]>fc_dens(y[i])/1.6){
+        y[i]<-runif(1,0,1)
+        u[i]<-runif(1,0,1)
+      }
+    }  
+  return (y)
 }
+
+tirage<-fc_methode_rejet(1000)
+
+h<-hist(tirage,breaks=50,plot=TRUE,freq = FALSE)
+
+par(new=TRUE)
+x<-seq(from=0,to=1,length=1000)
+z<-fc_dens(x)
+plot(x,z,type='l')
+
