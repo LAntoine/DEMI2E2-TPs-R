d214d0cb14c1516e8e24f1a27cb9df21f5e2a728
LAntoine <lepersantoine80@gmail.com>
2016-05-11 19:24:53 +0200

    still not finished but improved efficiency

--- ex20.R
+++ ex20.R
@@ -3,11 +3,11 @@ fc_tirage <- function(k){
   p<-rep(1/6,6)
   x<-1:6
   tirage<-matrix(sample(x=x,size=2*100,replace=TRUE,prob=p),ncol = 2)
-  for (i in 1:100){
-    if (tirage[i,1]>=k) tirage[i,2]<-tirage[i,1]
-  }
-  return (tirage[,2])
+  premier_tirage_bon<-tirage[,1]>=k
+  tirage[,1]<-premier_tirage_bon*tirage[,1] #on remplace par 0 tirage 1 si il est inférieur à k
+  premier_tirage_bon<-!(premier_tirage_bon)
+  return (tirage[,1]+premier_tirage_bon*tirage[,2]) #on renvoie la première colonne avec les deuxièmes tirages si necessaire
 }
 
+
 tirage<-fc_tirage(3)
-tirage
